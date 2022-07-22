9b700668bd60e7cd0a8e045211c5c4898a51164d
Roman <roman.makhanbetov@gmx.de>
2022-07-22 11:07:53 +0200

    2. Schritt commit

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -4,14 +4,17 @@ public class App {
     
     public static void main(String[] args) {
 
-        Cat cat1 = new Cat("Grizabella", "white", 29);
+        Cat cat1 = new Cat("Grizabella", "white", 29, true);
         output("Name: " + cat1.getStringAttributes("#name"));
         output("Fellfarbe " + cat1.getStringAttributes("#furColor"));
+        output("Alter: " + cat1.getAge());
+        output("Alter: " + cat1.getAge());
+        output("Alter: " + cat1.getAge());
         output("Alter: " + cat1.getAge());
 
         output("-------------");
 
-        Cat cat2 = new Cat("Alonzo", "grey", 35);
+        Cat cat2 = new Cat("Alonzo", "grey", 35, false);
         output("Name: " + cat2.getStringAttributes("#name"));
         output("Fellfarbe " + cat2.getStringAttributes("#furColor"));
         output("Alter: " + cat2.getAge());
--- com/cc/java/Cat.java
+++ com/cc/java/Cat.java
@@ -5,11 +5,15 @@ public class Cat {
     private String name;
     private String furColor;
     private int age;
+    private boolean isFemale;
+
+    private int counter;
    
-    public Cat(String name, String furColor, int age) {
+    public Cat(String name, String furColor, int age, boolean isFemale) {
         this.name = name;
         this.furColor = furColor;
         this.age = age;
+        this.isFemale = isFemale;
     }
 
     /** Getter */
@@ -24,10 +28,30 @@ public class Cat {
         }
     }
 
-    public int getAge() {
-        return age;
+    public String getAge() {
+        if (isFemale) {
+            return checkEscaliationLevel();
+        } else {
+            return String.valueOf(age);
+        }
     }
 
+    private String checkEscaliationLevel() {
+
+        counter++;
+
+        switch (counter) {
+            case 1:
+                return "This is an innapropriate question!";
+            case 2:
+                return "I've told you once!";
+            case 3:
+                return "Talk to the hand!";
+            default:
+                return "1#!?&&%";
+        }
+
+    }
 
 }
 
